7361a116508ccb4c90f7f2fe994e36bfa1c90b1c
Johannes Schildgen <yosi@schildix.de>
2020-05-06 07:53:27 +0200

    Changig language

--- js/language.js
+++ js/language.js
@@ -16,21 +16,44 @@ lang_de = {
     "Attribute": "Attribut",
     "Entity Type": "Enitätstyp",
     "Relationship": "Beziehung",
+    "Are you sure?": "Bist du dir sicher?",
+    "When you restart the game, you have to start all over again.": "Wenn du das Spiel neustartest, fängst du wieder ganz von vorne an.",
+    "Yes, restart!": "Ja, Neustart!",
+    "No, continue the game!": "Nein, weiterspielen!",
+    "Please choose your language.": "Bitte wähle eine Sprache.",
+    "Afterwards, you can continue your game without restarting.": "Danach kannst du das Spiel ganz normal weiterspielen, ohne es neu starten zu müssen.",
+    "%ex1": "Hallo! Hier gibt es noch nicht viel zu sehen. Probiere doch einfach die Funktionen aus und erstelle ein tolles ER-Diagramm.",
 }
 
-if(window.navigator.language.slice(0, 2) == "de") {
-    lang = lang_de;
-} else {
-    lang = lang_en;
+lang_en = {
+    "%ex1": "Hello! There's not much to see here yet. Why don't you just try out all features and create a great ER diagram."
 }
 
-$('.lang').each(function(e) {
-    this.innerHTML = translate(this.innerHTML);
-});
+change_language(window.navigator.language.slice(0, 2));
+
+function change_language(language_code) {
+    if(language_code == "de") {
+        lang = lang_de;
+    } else {
+        lang = lang_en;
+    }
+
+    $('.lang').each(function(e) {
+        if($(this).prop("data-lang") != undefined) {
+            this.innerHTML = translate($(this).prop("data-lang"));
+            return;
+        }
+        $(this).prop("data-lang", this.innerHTML);
+        this.innerHTML = translate(this.innerHTML);
+    });
+}
+
 
 function translate(txt) {
-    if(lang[txt] == undefined && window.navigator.language.slice(0, 2) != "en") {
-        console.log('"'+txt+'": "", ');
+    if(lang[txt] == undefined) {
+        if(window.navigator.language.slice(0, 2) != "en" || txt.slice(0,1) == "%") {
+            console.log('"' + txt + '": "", ');
+        }
         return txt;
     }
     return lang[txt];
